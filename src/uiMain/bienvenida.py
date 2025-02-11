ef498e9eb7f2f54e64f1ab931cd561fe7d697ad3
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-02-10 21:38:42 -0500

    bordes
    Hay problemas con el supuesto margen entre el cuadro de la isquierda y el de la derecha

--- src/uiMain/bienvenida.py
+++ src/uiMain/bienvenida.py
@@ -12,25 +12,34 @@ class Aplication(tk.Frame):
     def create_widgets(self):
         self.titulo = tk.Label(self)
         self.titulo["font"] = ("Arial", 30)
-        self.titulo["text"] = "Bienvenido"
-        self.titulo.grid(row = 0, column = 0, sticky="sew",columnspan=2)
+        self.titulo["text"] = "Inicio"
+        self.titulo.grid(row = 0, column = 0,columnspan=2, sticky="w")
 
         self.infoSistema = infoSistema(window = self.master, master = self)
         self.infoSistema.grid(row = 1, column = 1, sticky="sewn")
         self.columnconfigure(1,weight=10)
 
+        # La columna 2 sirve de margen entre infoSistema y infoDesarrolladores
+        # Y para funcionar como tal, debe tener algo.
+        self.columna2 = tk.Label(master = self)
+        self.columna2.grid(row = 1, column = 2,sticky="sewn")
+        self.columnconfigure(2,weight=1)
+
         self.infoDesarrolladores = infoDesarrolladores(self.master, master = self)
         self.infoDesarrolladores.grid(row = 1, column = 2,sticky="sewn")
-        self.columnconfigure(2,weight=10)
+        self.columnconfigure(3,weight=10)
 
-        self.rowconfigure(0,weight=1)
-        self.rowconfigure(1,weight=9)
+        self.columnconfigure(4,weight=1)
+        self.columnconfigure(0,weight=1)
+
+        
 
 class infoDesarrolladores(tk.Frame):
     def __init__(self,window, master=None):
         super().__init__(master)
         self.master = master
         self.window = window
+        self.config(highlightbackground="black",highlightthickness=2)
         self.create_widgets()
 
     def create_widgets(self):
@@ -40,7 +49,7 @@ Pero entre esquina y esquina Siempre habrá lo mismo
 Para mi no existe el cielo Ni Luna ni estrellas
 Para mi no alumbra el Sol Pa' mi todo es tinieblas"""
         self.desarrolladores = tk.Label(master = self, text=hojaDeVida)
-        self.desarrolladores.grid(row = 0, column = 0)
+        self.desarrolladores.grid(row = 0, column = 0, padx=10, pady=10)
         self.bettyYElOtro = tk.PhotoImage(master = self.window, file = f"{os.getcwd()}\\src\\uiMain\\imagenes\\bettyYElOtro.png")
         self.abajo = tk.Label(master = self, image =self.bettyYElOtro) 
         self.abajo.grid(row = 1, column = 0)
@@ -54,14 +63,20 @@ class infoSistema(tk.Frame):
         self.master = master
         self.window = window
         self.create_widgets()
+        self.config(highlightbackground="black",highlightthickness=2)
 
     def create_widgets(self):
+        # P3 en el enunciado de la practica
+        frameArriba = tk.Frame(master = self, highlightbackground="black",highlightthickness=2)
         mensaje = """Bienvenido a ecomoda, donde aplicamos teoría y practica:
 teoría es cuando sabemos todo pero nada funciona, y la practica es cuando
 todo funciona pero no se sabe porqué. En ecomoda, juntamos la teoría y la practica: Nada
 funciona, y no sabemos porqué."""
-        self.saludo = tk.Label(master = self, text=mensaje)
+        self.saludo = tk.Label(frameArriba, text=mensaje)
         self.saludo.grid(row = 0, column = 0)
+        
+        frameArriba.grid(row = 0, column = 0, padx=10, pady=10)
+
         self.parteAbajo = p4FotosEInicio(self.window ,master = self)
         self.parteAbajo.grid(row = 1, column = 0)
 
@@ -75,6 +90,7 @@ class p4FotosEInicio(tk.Frame):
         self.master = master
         self.window = window
         self.create_widgets()
+        self.config(highlightbackground="black",highlightthickness=2, padx=10, pady=10)
 
     def create_widgets(self):
         self.perroCosiendo = tk.PhotoImage(master=self.window, file = f"{os.getcwd()}\\src\\uiMain\\imagenes\\perroCosiendo.png")
