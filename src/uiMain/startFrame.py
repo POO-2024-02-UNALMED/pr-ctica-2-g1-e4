8e2bc05b9f7e1215cbd46389ab28a2d2bf4fa5d1
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-02-11 20:02:30 -0500

    arreglar ingreso de fecha
    Co-Authored-By: Andrea Merino Mesa <amerino@unal.edu.co>
    Co-Authored-By: Jackelin_Lozano_Blanquiceth <186767271+JackelinBlanquiceth@users.noreply.github.com>
    Co-Authored-By: LuisRincon16 <186876517+LuisRincon16@users.noreply.github.com>
    Co-Authored-By: Valen2525 <181977083+Valen2525@users.noreply.github.com>

--- src/uiMain/startFrame.py
+++ src/uiMain/startFrame.py
@@ -1,8 +1,11 @@
+# Dibuja la ventana y la parte externa, y la parte interna la saca de las clases F.
+# O en el caso respectivo, no dibuja una funcionalidad, sino principalInicial.
 import os
 import tkinter as tk
 from tkinter.font import Font
 import sys
 from src.uiMain.main import Main
+from src.uiMain.frameInicial import frameInicial
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 # Inicializar pygame para el audio
@@ -33,78 +36,8 @@ procesosButton.place(relx=0.1, rely=0.5, relwidth=0.2, relheight=1, anchor="w")
 ayudaButton = tk.Button(opciones, text="Ayuda")
 ayudaButton.place(relx=0.3, rely=0.5, relwidth=0.1, relheight=1, anchor="w")
 
-inicio =  tk.Frame(ventana, bg="blue")
-inicio.pack(fill="both", expand=True, padx=7, pady=7)
-
-primerInicio = tk.Frame(inicio, height=150)
-primerInicio.pack(side="top", fill="x")
-
-#lbl_font = Font(family="Roboto Cn", size=17) 
-
-label1 = tk.Label(primerInicio, text="Sistema Operativo de Ecomoda", bg="medium orchid", relief="ridge", font=("Arial",16, "bold"))
-label1.place(relx=0.5, rely=0.6, relwidth=1, relheight=0.6, anchor="s") 
-## relwidth y relheight reciben el porcentaje de tamaño respecto al contenedor
-
-label2 = tk.Label(primerInicio, text="Realiza un proceso de facturación, surte insumos, produce prendas, gestiona a tus empleados y revisa el estado financiero de tu empresa :)", relief="ridge")
-label2.place(relx=1, rely=0.8, relwidth=1, relheight=0.4, anchor="e")
-
-pedirFecha = tk.Frame(inicio, bg="light gray")
-pedirFecha.pack(fill="both", expand=True, anchor="s")
-
-label3 = tk.Label(
-    pedirFecha, 
-    text="\nPuedes hacerlo a través de la opción: Procesos y Consultas >>", 
-    relief="ridge", 
-    anchor="n",  # Asegura que el texto esté alineado arriba
-    justify="center",  # Centra el texto horizontalmente
-)
-label3.place(relx=0.5, rely=0, relwidth=1, relheight=0.7, anchor="n")
-
-logo = tk.PhotoImage(master=label3, file=f"{os.getcwd()}\\src\\uiMain\\imagenes\\logoEcomoda.png")
-
-# Redimensionar la imagen usando subsample()
-# La imagen será reducida al tamaño deseado sin recortes
-logo_resized = logo.subsample(2, 2)  
-
-# Crear el label con la imagen redimensionada
-foto = tk.Label(master=label3, image=logo_resized, bg="light gray")
-foto.image = logo_resized  # Mantener la referencia de la imagen
-foto.place(relx=0.5, rely=0.24, anchor="n")
-
-label4 = tk.Label(pedirFecha, text="Para iniciar ingresa la fecha de hoy ", relief="ridge", anchor="w")
-label4.place(relx=0.5, rely=0.7, relwidth=1, relheight=0.3, anchor="n")
-label4.config(padx=200)  
-
-entradaDia =tk.Entry(pedirFecha, bg="plum3")
-entradaDia.place(relx=0.55, rely=0.8, relwidth=0.06, relheight=0.1, anchor="n")
-entradaDia.insert(0,"d/ ")
-entradaMes =tk.Entry(pedirFecha, bg="plum3")
-entradaMes.place(relx=0.615, rely=0.8, relwidth=0.06, relheight=0.1, anchor="n")
-entradaMes.insert(0,"m/ ")
-entradaAño =tk.Entry(pedirFecha, bg="plum3")
-entradaAño.place(relx=0.6849, rely=0.8, relwidth=0.07, relheight=0.1, anchor="n")
-entradaAño.insert(0,"a/ ")
-
-# Función que se ejecutará al presionar el botón
-def Ok(event):
-    global entradaDia, entradaMes, entradaAño
-    # Leer los valores de las entradas
-    FDia = entradaDia.get() # Obtener el texto de la entrada para el día
-    FMes = entradaMes.get() # Obtener el texto de la entrada para el mes
-    FAño = entradaAño.get() # Obtener el texto de la entrada para el año
-    fecha=Main.ingresarFecha(FDia,FMes,FAño)
-    pass
-
-def borrar():
-    entradaDia.delete(0, tk.END)
-    entradaMes.delete(0, tk.END)
-    entradaAño.delete(0, tk.END)
-    entradaDia.insert(0,"d/ ")
-    entradaMes.insert(0,"m/ ")
-    entradaAño.insert(0,"a/ ")
-
-boton1=tk.Button(pedirFecha,text="Enviar")
-boton1.place(relx=0.820, rely=0.8, relwidth=0.1, relheight=0.1, anchor="n")
-boton1.bind("<Button-1>", Ok)
+areaPrincipal = frameInicial(ventana) # Será eliminado y reemplazado par alguna funcionalidad, cuando se use una 
+# de las opciones de procesosButton
+areaPrincipal.pack(fill="both", expand=True, padx=7, pady=7)
 
 ventana.mainloop()
