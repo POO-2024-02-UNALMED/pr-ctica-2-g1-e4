5217e1a1751fc67971676a36820310054acc1e78
Valen2525 <juroseror@unal.edu.co>
2025-02-18 18:10:25 -0500

    Update F2Insumos.py

--- src/uiMain/F2Insumos.py
+++ src/uiMain/F2Insumos.py
@@ -8,45 +8,43 @@ from src.gestorAplicacion.bodega.pantalon import Pantalon
 from src.gestorAplicacion.sede import Sede
 from src.gestorAplicacion.venta import Venta
 
-class F2Insumos(tk.Frame):
 
-    def __init__(self,master):
-        super().__init__(master)
-        self.pesimismo()
+def pesimismo(self):
+    from src.uiMain.main import Main
+    from src.uiMain import fieldFrame, main
+    criterios = []
+    valores = []
 
-    def pesimismo(self):
-        from src.uiMain.main import Main
-        from src.uiMain import fieldFrame, main
-        criterios = []
-        valores = []
+    framePrincipal =  tk.Frame(self, bg="blue")
+    framePrincipal.pack(fill="both", expand=True, padx=7, pady=7)
 
-        framePrincipal =  tk.Frame(self, bg="blue")
-        framePrincipal.pack(fill="both", expand=True, padx=7, pady=7)
+    frame1 = tk.Frame(framePrincipal, height=150)
+    frame1.pack(side="top", fill="x")
 
-        frame1 = tk.Frame(framePrincipal, height=150)
-        frame1.pack(side="top", fill="x")
-
-        tituloF2 = tk.Label(frame1, text="Surtir Insumos", bg="medium orchid", relief="ridge", font=("Arial",16, "bold"))
-        tituloF2.place(relx=0.5, rely=0.6, relwidth=1, relheight=0.6, anchor="s") 
+    tituloF2 = tk.Label(frame1, text="Surtir Insumos", bg="medium orchid", relief="ridge", font=("Arial",16, "bold"))
+    tituloF2.place(relx=0.5, rely=0.6, relwidth=1, relheight=0.6, anchor="s") 
 
         ## relwidth y relheight reciben el porcentaje de tamaño respecto al contenedor
-        descripcionF2 = tk.Label(frame1, text="Registra la llegada de nuevos insumos: Incluye una predicción de ventas del siguiente mes para hacer la compra de los insumos, actualiza la deuda con los proveedores y añade los nuevos insumos a la cantidad en Stock.", relief="ridge",wraplength=600)
-        descripcionF2.place(relx=1, rely=0.8, relwidth=1, relheight=0.4, anchor="e")
+    descripcionF2 = tk.Label(frame1, text="Registra la llegada de nuevos insumos: Incluye una predicción de ventas del siguiente mes para hacer la compra de los insumos, actualiza la deuda con los proveedores y añade los nuevos insumos a la cantidad en Stock.", relief="ridge",wraplength=600)
+    descripcionF2.place(relx=1, rely=0.8, relwidth=1, relheight=0.4, anchor="e")
 
-        frame2 = tk.Frame(framePrincipal, bg="light gray")
-        frame2.pack(anchor="s",  expand=True, fill="both")
+    frame2 = tk.Frame(framePrincipal, bg="light gray")
+    frame2.pack(anchor="s",  expand=True, fill="both")
         
-        for s in Sede.getListaSedes():
-            criterios.append(s)
-            valores.append(f"{round(Venta.getPesimismo()*100)}%")
-        field = fieldFrame.FieldFrame(frame2, "Puede cambiar la prediccion de ventas para el siguiente mes...", criterios, "", valores, [True, True])
-        field.pack(anchor="s",  expand=True, fill="both")
-        fecha =main.Main.fecha
-        pantalonesPredichos = False
-        camisasPredichas = False
+    for s in Sede.getListaSedes():
+        criterios.append(s)
+        valores.append(f"{round(Venta.getPesimismo()*100)}%")
+    field = fieldFrame.FieldFrame(frame2, "Puede cambiar la prediccion de ventas para el siguiente mes...", criterios, "", valores, [True, True])
+    field.pack(anchor="s",  expand=True, fill="both")
+    fecha =main.Main.fecha
+    pantalonesPredichos = False
+    camisasPredichas = False
      
 
-        #Main.planificarProduccion(main.Main.fecha, frame2)
-    def prediccion(frame2, sede, prenda, prediccion):
-        prediccion = tk.Label(frame2,text="\nLa predicción de ventas para " + str(prenda) + " es de " + str(math.ceil(prediccion)) + " en la "+ str(sede))
-        prediccion.place(relx=1, rely=0, relwidth=1, relheight=0.4, anchor="e")
+#Main.planificarProduccion(main.Main.fecha, frame2)
+def prediccion(frame2, sede, prenda, prediccion):
+    prediccion = tk.Label(frame2,text="\nLa predicción de ventas para " + str(prenda) + " es de " + str(math.ceil(prediccion)) + " en la "+ str(sede))
+    prediccion.place(relx=1, rely=0, relwidth=1, relheight=0.4, anchor="e")
+
+
+tk.mainloop()
