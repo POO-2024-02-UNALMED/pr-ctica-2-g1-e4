9c74310d74ce049e499344781524edf752dfc590
JackelinBlanquiceth <glozanob@unal.edu.co>
2025-02-19 07:38:00 -0500

    Excepciones como deben ser :p

--- src/uiMain/Excepciones/exceptionC1.py
+++ src/uiMain/Excepciones/exceptionC1.py
@@ -2,7 +2,23 @@ from tkinter import messagebox
 from src.uiMain.Excepciones.errorAplicacion import ErrorAplicacion
 class ExceptionC1(ErrorAplicacion):
     def __init__(self, mensaje):
-        super().__init__(mensaje)
+        self.mensajeValoresNovalidos=f" Error de entrada de dato: {mensaje} "
+        super().__init__( self.mensajeValoresNovalidos)
+
+class ExcepcionEnteroNoValido(ExceptionC1):
+     def __init__(self, valor):
+        self.mensajeValor=f" La entrada {valor} no contiene un valor válido, por favor, intetar de nuevo "
+        super().__init__( self.mensajeValor)
+
+class ExcepcionContenidoVacio(ExceptionC1):
+    def __init__(self, entradas):
+        self.mensajeEntradas=f" Necesita llenar la(s) entrada(s) {entradas} para continuar"
+        super().__init__( self.mensajeEntradas)
+
+class ExcepcionStringNoEntero(ExceptionC1):
+    def __init__(self, entero):
+        self.mensajeEntero=f" La entrada {entero} no es válida, debe llenar este campo solo con String"
+        super().__init__( self.mensajeEntero)
 
     def enteroNoValido(self):
        messagebox.showwarning("Alerta", f"{self}")
@@ -13,7 +29,7 @@ class ExceptionC1(ErrorAplicacion):
     
    # def stringNoEntero(self, string):
 
-   
+
 
     
 
--- src/uiMain/Excepciones/exceptionC2.py
+++ src/uiMain/Excepciones/exceptionC2.py
@@ -3,9 +3,16 @@ from src.uiMain.Excepciones.errorAplicacion import ErrorAplicacion
 
 class ExceptionC2(ErrorAplicacion):
     def __init__(self, mensaje):
-        super().__init__(mensaje)
+        self.mensajeCompleto = f" Error: {mensaje}"
+        super().__init__(self.mensajeCompleto)
 
-    def codigoTarjetaRegalo(self, codigo):
-        messagebox.askyesno("Notificación", f" El código {codigo} no se encuentra habilitado. ¿Desea intentar nuevamente?")
-        raise self #Posible excepción
+class ExcepcionCodigoTarjetaregalo(ExceptionC2):
+    def __init__(self, codigo):
+        self.mensajeCodigo = f" El código {codigo} no se encuentra habilitado. ¿Desea intentar nuevamente?"
+        super().__init__(self.mensajeCodigo)
+
+class ExcepcionPrendaNoExistente(ExceptionC2):
+    def __init__(self, prenda):
+        self.mensajePrenda = f" La prenda {prenda} no es vendida en nuestra empresa. ¿Desea elegir una prenda nuevamente?"
+        super().__init__(self.mensajePrenda)
     
