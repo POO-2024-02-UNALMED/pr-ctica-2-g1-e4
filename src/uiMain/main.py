4dd58f2540116ea18d97bde35d1fca3b7cec262c
Andres David Calderon <david.voidgames@gmail.com>
2025-02-25 13:27:37 -0500

    final insumos

--- src/uiMain/main.py
+++ src/uiMain/main.py
@@ -501,6 +501,7 @@ class Main:
             for idxInsumo, insumo in enumerate(sede[0]):
                 cantidadExtra=cantidadesExtra[idxInsumoExtra]
                 cls.comprarInsumo(sede[1][idxInsumo]+cantidadExtra, insumo, insumo.getProveedor(), Sede.getListaSedes()[cls.extraPorComprar.index(sede)])
+        return True
     
     #endregion
 #region Producción    
